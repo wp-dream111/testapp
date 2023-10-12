5021e6dacac6edd585a5e6ff15e43ffd7ce0507c
Brightness999 <brightness0804@gmail.com>
2023-10-12 12:31:24 -0400

    remove unused files

--- constants/images.js
+++ constants/images.js
@@ -1,10 +1,4 @@
-export const beach = require('../assets/images/beach.jpeg');
-export const climbingHills = require('../assets/images/climbing_hills.jpg');
-export const frozenHills = require('../assets/images/frozen_hills.jpg');
-export const homeBanner = require('../assets/images/home_banner.jpg');
 export const onBoardingImage = require('../assets/images/onboarding_image.jpg');
-export const skiVilla = require('../assets/images/ski_villa.jpg');
-export const skiVillaBanner = require('../assets/images/ski_villa_banner.jpg');
 export const homeround = require('../assets/images/homeround.png');
 export const loading = require('../assets/images/loading.png');
 export const golden_flower = require('../assets/images/golden_flower.png');
@@ -13,14 +7,8 @@ export const apple_pay = require('../assets/images/apple_pay.png');
 export const chase = require('../assets/images/chase.png');
 
 export default {
-  beach,
-  climbingHills,
-  frozenHills,
   loading,
-  homeBanner,
   onBoardingImage,
-  skiVilla,
-  skiVillaBanner,
   homeround,
   golden_flower,
   united,
